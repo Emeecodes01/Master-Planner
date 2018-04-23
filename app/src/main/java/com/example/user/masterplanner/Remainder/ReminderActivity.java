45bc4ab954dab719eabadf04119e367a37d61122
Emeecodes01 <emmanuel022forreal@gmail.com>
2018-04-23 02:01:45 -1200

    I have set up the main design, am going to use FDP, next will be sction bar sync with navigation menu

--- app/src/main/java/com/example/user/masterplanner/Remainder/ReminderActivity.java
+++ app/src/main/java/com/example/user/masterplanner/Remainder/ReminderActivity.java
@@ -2,7 +2,13 @@ package com.example.user.masterplanner.Remainder;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import com.example.user.masterplanner.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -13,6 +19,11 @@ import butterknife.ButterKnife;
 
 public class ReminderActivity extends AppCompatActivity{
 
+    @BindView(R.id.nav_drawer)
+    private NavigationView navigationView;
+    @BindView(R.id.drawer_layout)
+    private DrawerLayout drawer_layout;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,4 +33,14 @@ public class ReminderActivity extends AppCompatActivity{
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/example/user/masterplanner/Remainder/ReminderFragment.java
+++ app/src/main/java/com/example/user/masterplanner/Remainder/ReminderFragment.java
@@ -1,22 +1,78 @@
 package com.example.user.masterplanner.Remainder;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.example.user.masterplanner.Models.Remainder;
+import com.example.user.masterplanner.R;
 
 import java.util.List;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by emmanuel on 2018-04-20.
  */
 
-public class ReminderFragment implements RemainderView{
+public class ReminderFragment extends Fragment implements RemainderView, View.OnClickListener{
+    private OnRemainderFragmentAttach fragmentAttach;
+
+    @BindView(R.id.addFab)
+    private FloatingActionButton addFab;
 
     @Inject
     Presenter presenter;
 
     @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            fragmentAttach = (OnRemainderFragmentAttach) context;
+        }catch (ClassCastException e){
+            throw new ClassCastException("Can't cast this shitty class: piss off man");
+        }
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_reminder_content, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        addFab.setOnClickListener(this);
+    }
+
+
+    @Override
     public void displayReminders(List<Remainder> remainderList) {
         //todo: display data
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.addFab:
+                fragmentAttach.onAddReminderClicked();
+                break;
+        }
+    }
+
+    interface OnRemainderFragmentAttach{
+        void onAddReminderClicked();
     }
 }
